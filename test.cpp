013d9041bc9b2466c2dc7fa239f65af27dfe8c5c
Marysiako <130159052+Marysiako@users.noreply.github.com>
2023-12-02 20:25:03 +0100

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
-#include <portaudio.h>
+#include <vector>
+#include "portaudio.h"
 
 const int SAMPLE_RATE = 44100;
 const int FRAME_SIZE = 1024;
+const double PI = 3.14159265358979323846;
 
+// Funkcja do obliczania częstotliwości na podstawie próbek dźwięku.
 double CalculateFrequency(const std::vector<double>& samples) {
     double maxAmplitude = 0.0;
     int maxIndex = 0;
@@ -23,43 +25,56 @@ double CalculateFrequency(const std::vector<double>& samples) {
     return frequency;
 }
 
+// Callback dla PortAudio do przetwarzania próbek dźwięku.
+int RecordCallback(const void* inputBuffer, void* outputBuffer, unsigned long framesPerBuffer,
+                   const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void* userData) {
+    const float* in = static_cast<const float*>(inputBuffer);
+    std::vector<double> samples(FRAME_SIZE);
+
+    for (int i = 0; i < FRAME_SIZE; ++i) {
+        samples[i] = static_cast<double>(in[i]);
+    }
+
+    double frequency = CalculateFrequency(samples);
+
+    // Prosta detekcja tonu stroikiem gitary.
+    if (frequency > 80.0 && frequency < 1500.0) {
+        std::cout << "Aktualna częstotliwość: " << frequency << " Hz" << std::endl;
+
+        // Możesz dodać kod do porównywania częstotliwości z oczekiwanymi wartościami tonów gitary
+        // i informować użytkownika, czy gitara jest dobrze nastrojona.
+    }
+
+    return paContinue;
+}
+
 int main() {
     PaError err;
 
-    // Inicjalizacja PortAudio.
     err = Pa_Initialize();
     if (err != paNoError) {
         std::cerr << "Błąd inicjalizacji PortAudio: " << Pa_GetErrorText(err) << std::endl;
-        return 1;
+        return -1;
     }
 
-    // Otwarcie strumienia wejściowego.
-    PaStream *stream;
-    err = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, SAMPLE_RATE, FRAME_SIZE, NULL, NULL);
+    PaStream* stream;
+    err = Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, SAMPLE_RATE, FRAME_SIZE, RecordCallback, nullptr);
     if (err != paNoError) {
         std::cerr << "Błąd otwierania strumienia: " << Pa_GetErrorText(err) << std::endl;
-        return 1;
+        Pa_Terminate();
+        return -1;
     }
 
     err = Pa_StartStream(stream);
     if (err != paNoError) {
         std::cerr << "Błąd uruchamiania strumienia: " << Pa_GetErrorText(err) << std::endl;
-        return 1;
+        Pa_CloseStream(stream);
+        Pa_Terminate();
+        return -1;
     }
 
-    std::vector<double> samples(FRAME_SIZE);
-
-    while (true) {
-        err = Pa_ReadStream(stream, samples.data(), FRAME_SIZE);
-        if (err != paNoError) {
-            std::cerr << "Błąd odczytu danych z mikrofonu: " << Pa_GetErrorText(err) << std::endl;
-            break;
-        }
-
-        double frequency = CalculateFrequency(samples);
-
-        std::cout << "Dominująca częstotliwość: " << frequency << " Hz" << std::endl;
-    }
+    std::cout << "Naciśnij Enter, aby zakończyć strojenie gitary..." << std::endl;
+    std::cin.get();
 
     err = Pa_StopStream(stream);
     if (err != paNoError) {
@@ -71,7 +86,6 @@ int main() {
         std::cerr << "Błąd zamykania strumienia: " << Pa_GetErrorText(err) << std::endl;
     }
 
-    // Terminate PortAudio.
     Pa_Terminate();
 
     return 0;
